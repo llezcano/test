472fe96e77557d46df72c3049a6f8cfae2821df6
llezcano <leandro.c.lezcano@gmail.com>
2014-05-12 13:10:30 -0300

    adding HelloWorld class

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+class HelloWorld {
+	public integer key ;
+	// Method say hello print "HelloWorld" on console
+	public void sayHello () {
+		System.out.println("HelloWorld") ;
+		String aStr = "hola"
+		// this is a comment
+		if (aStr == "hola") {
+			break ;
+		} 
+	}
+
+}
