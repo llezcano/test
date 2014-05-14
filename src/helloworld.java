68ea49b4c4fd9e3767cf54bca920a5585c4e9638
llezcano <leandro.c.lezcano@gmail.com>
2014-05-14 09:31:48 -0300

    prueba

--- src/helloworld.java
+++ src/helloworld.java
@@ -3,7 +3,7 @@ class HelloWorld {
 	// Method say hello print "HelloWorld" on console
 	public void sayHello () {
 		System.out.println("HelloWorld") ;
-		String aStr = "hola"
+		String aStr = "hola";
 		// this is a comment
 		if (aStr == "hola") {
 			break ;
